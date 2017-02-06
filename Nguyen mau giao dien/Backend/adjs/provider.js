5d8d63cbbf85d7efff7e2915e3552732845aa4a6
aceonlinevn <spacecowboyvn@gmail.com>
2017-02-06 20:03:47 +0700

    recovery

---
+++ Nguyen mau giao dien/Backend/adjs/provider.js
@@ -0,0 +1,19 @@
+﻿/*The script processing for provider.html*/
+
+function setForm(fn){
+	fn.btnRegistry.disabled = true;
+	fn.reset();
+}
+
+function checkValidProvider(fn){
+	var name = fn.txtProviderName.value;
+	var check = fn.chkAgree.checked;
+	
+	//Kiểm tra trạng thái
+	name = name.trim();
+	if(name !="" && check){
+		fn.btnRegistry.disabled=false;
+	}else{
+		fn.btnRegistry.disabled=true;
+	}	
+}
