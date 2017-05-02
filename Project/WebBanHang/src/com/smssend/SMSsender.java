812fb1bcd1d69f8911cce58c0142b1ca6dc5706c
aceonlinevn <spacecowboyvn@gmail.com>
2017-05-02 18:51:03 +0700

    update send sms

--- Project/WebBanHang/src/com/smssend/SMSsender.java
+++ Project/WebBanHang/src/com/smssend/SMSsender.java
@@ -1,4 +1,5 @@
 package com.smssend;
+
 import java.io.*;
 import java.util.*;
 import gnu.io.*;
@@ -9,9 +10,9 @@ public class SMSsender {
 	static String messageString1 = "AT";
 	static String messageString2 = "AT+CPIN=\"7078\"";
 	static String messageString3 = "AT+CMGF=1";
-	static String messageString4 = "AT+CMGS=\"+841292723597\"";
+	//static String messageString4 = "AT+CMGS=\"+841292723597\"";
 
-	static String messageString5 = "Xin chao, Day la tin nhan thu nghiem cua chuc nang nhan tin tu dong";
+	//static String messageString5 = "Xin chao, Day la tin nhan thu nghiem cua chuc nang nhan tin tu dong";
 	static SerialPort serialPort;
 	static OutputStream outputStream;
 	static InputStream inputStream;
@@ -19,7 +20,9 @@ public class SMSsender {
 
 	static char CTRLZ = 26;
 
-	public static void main(String[] args) throws InterruptedException {
+	public static boolean SmsSender(String phonenumber, String message) {
+		String messageString4 = "AT+CMGS=\""+phonenumber+"\"";
+		boolean flag = true;
 		portList = CommPortIdentifier.getPortIdentifiers();
 
 		while (portList.hasMoreElements()) {
@@ -33,18 +36,21 @@ public class SMSsender {
 						serialPort = (SerialPort) portId.open("COM4", 2000);
 					} catch (PortInUseException e) {
 						System.out.println("err");
+						flag = false;
 					}
 					try {
 						outputStream = serialPort.getOutputStream();
 						inputStream = serialPort.getInputStream();
 					} catch (IOException e) {
 						e.printStackTrace();
+						flag = false;
 					}
 					try {
 						serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
 								SerialPort.PARITY_NONE);
 					} catch (UnsupportedCommOperationException e) {
 						e.printStackTrace();
+						flag = false;
 					}
 					try {
 
@@ -53,7 +59,6 @@ public class SMSsender {
 						Thread.sleep(100);
 						outputStream.flush();
 
-						
 						outputStream.write((messageString3 + enter).getBytes());
 
 						Thread.sleep(100);
@@ -64,7 +69,7 @@ public class SMSsender {
 						Thread.sleep(100);
 						outputStream.flush();
 
-						outputStream.write((messageString5 + CTRLZ).getBytes());
+						outputStream.write((message + CTRLZ).getBytes());
 
 						outputStream.flush();
 						Thread.sleep(100);
@@ -76,11 +81,81 @@ public class SMSsender {
 						serialPort.close();
 						System.out.println("Port COM zamkniety");
 
-					} catch (IOException e) {
+					} catch (IOException | InterruptedException e) {
 						e.printStackTrace();
+						flag = false;
 					}
+				} else {
+					flag = false;
 				}
+			} else {
+				flag = false;
 			}
 		}
+		return flag;
 	}
+
+//	public static void main(String[] args) throws InterruptedException {
+//		portList = CommPortIdentifier.getPortIdentifiers();
+//
+//		while (portList.hasMoreElements()) {
+//
+//			portId = (CommPortIdentifier) portList.nextElement();
+//			if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+//
+//				if (portId.getName().equals("COM4")) {
+//
+//					try {
+//						serialPort = (SerialPort) portId.open("COM4", 2000);
+//					} catch (PortInUseException e) {
+//						System.out.println("err");
+//					}
+//					try {
+//						outputStream = serialPort.getOutputStream();
+//						inputStream = serialPort.getInputStream();
+//					} catch (IOException e) {
+//						e.printStackTrace();
+//					}
+//					try {
+//						serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1,
+//								SerialPort.PARITY_NONE);
+//					} catch (UnsupportedCommOperationException e) {
+//						e.printStackTrace();
+//					}
+//					try {
+//
+//						outputStream.write((messageString1 + enter).getBytes());
+//
+//						Thread.sleep(100);
+//						outputStream.flush();
+//
+//						outputStream.write((messageString3 + enter).getBytes());
+//
+//						Thread.sleep(100);
+//						outputStream.flush();
+//
+//						outputStream.write((messageString4 + enter).getBytes());
+//
+//						Thread.sleep(100);
+//						outputStream.flush();
+//
+//						outputStream.write((messageString5 + CTRLZ).getBytes());
+//
+//						outputStream.flush();
+//						Thread.sleep(100);
+//
+//						System.out.println("Wyslano wiadomosc");
+//						Thread.sleep(3000);
+//
+//						outputStream.close();
+//						serialPort.close();
+//						System.out.println("Port COM zamkniety");
+//
+//					} catch (IOException e) {
+//						e.printStackTrace();
+//					}
+//				}
+//			}
+//		}
+//	}
 }
